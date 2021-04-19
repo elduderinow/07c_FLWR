fca93fc4af3e734af17794a670d2e3ad39b0e9eb
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-04-19 09:14:10 +0200

    updates to homepage from the weekend

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,12 @@
+/* --- HOME - LOGO MOVE ON SCROLL ---*/
+
 const header = document.querySelector('header');
 const logoSmall = document.querySelector('#logoSmall');
 
 window.onscroll = function(){
     let top = window.scrollY;
     console.log(top);
-    if (top >= 700){
+    if (top >= 600){
         logoSmall.classList.remove('logoSmallHide');
         logoSmall.classList.add('logoSmallShow');
     }else{
